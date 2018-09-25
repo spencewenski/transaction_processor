bc4e77212ddc856e1beb02ec1316c3841fe02bf4
Spencer Ferris <sferris37@gmail.com>
2018-09-24 22:48:43 -0700

    Validate config file and
    Issue #26: Input/config validation
    
    Validate user provided config files to verify that, for example, payee
    ids exist when referenced from payee matchers. Also verify that account
    and format command line arguments exist in the specified config file
    
    Also fix bug where destination format is used as the source format

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,6 +4,8 @@ use parser::{deserialize_keyed_items, Keyed, default_false, default_true};
 use std::collections::{HashMap};
 use config::arguments::{Arguments};
 use util;
+use regex::RegexBuilder;
+use itertools::Itertools;
 
 mod arguments;
 
@@ -17,6 +19,7 @@ impl Config {
         let args = Arguments::parse_args();
         let r = util::reader_from_file_name(&args.config_file)?;
         let config_file = ConfigFile::from_reader(r)?;
+        validate_args(&args, &config_file)?;
         Ok(Config {
             args,
             config_file,
@@ -29,6 +32,12 @@ impl Config {
 
     pub fn account(&self) -> Option<&AccountConfig> {
         self.config_file.accounts.get(self.account_id())
+    }
+
+    pub fn src_format(&self) -> Option<&FormatConfig> {
+        self.account().and_then(|a| {
+            self.config_file.formats.get(&a.format_id)
+        })
     }
 
     fn dst_format_id(&self) -> &str {
@@ -87,6 +96,18 @@ impl Config {
     }
 }
 
+fn validate_args(args: &Arguments, c: &ConfigFile) -> Result<(), String> {
+    if !c.accounts.contains_key(&args.src_account) {
+        return Err(format!("Source account id [{}] specified on command line does not exist in config file. Available options are [{}].",
+                           args.src_account, c.accounts.iter().map(|(_, a)| &a.id).sorted().iter().join(", ")));
+    }
+    if !c.formats.contains_key(&args.dst_format) {
+        return Err(format!("Destination format id [{}] specified on command line does not exist in config file. Available options are [{}].",
+                           args.dst_format, c.formats.iter().map(|(_, f)| &f.id).sorted().iter().join(", ")));
+    }
+    Ok(())
+}
+
 #[derive(Debug, Deserialize)]
 struct ConfigFile {
     #[serde(rename = "accounts", deserialize_with = "deserialize_keyed_items")]
@@ -106,12 +127,16 @@ struct ConfigFile {
 impl ConfigFile {
     fn from_reader(r: Box<io::Read>) -> Result<ConfigFile, String> {
         match serde_json::from_reader(r) {
-            Ok(c) => Self::verify(c),
+            Ok(c) => Self::validate(c),
             Err(e) => Err(format!("Unable to read config file: {}", e)),
         }
     }
 
-    fn verify(c: ConfigFile) -> Result<ConfigFile, String> {
+    fn validate(c: ConfigFile) -> Result<ConfigFile, String> {
+        validate_accounts(&c)?;
+        validate_payee_normalizer_configs(&c)?;
+        validate_payees(&c)?;
+        validate_formats(&c)?;
         Ok(c)
     }
 }
@@ -142,6 +167,16 @@ impl Keyed<String> for AccountConfig {
     }
 }
 
+fn validate_accounts(c: &ConfigFile) -> Result<(), String> {
+    for (a_id, a) in &c.accounts {
+        if let Option::None = c.formats.get(&a.format_id) {
+            return Err(format!("Format with id [{}] does not exist. The id is referenced from account with id [{}]",
+                               a.format_id, a_id));
+        }
+    }
+    Ok(())
+}
+
 #[derive(Debug, Deserialize)]
 pub struct PayeeNormalizerConfig {
     #[serde(rename = "matcher")]
@@ -150,6 +185,24 @@ pub struct PayeeNormalizerConfig {
     pub payee_id: String,
     #[serde(rename = "ignoreCase", default = "default_true")]
     pub ignore_case: bool,
+}
+
+fn validate_payee_normalizer_configs(c: &ConfigFile) -> Result<(), String> {
+    for (a_id, a) in &c.accounts {
+        for n in &a.payee_normalizers {
+            if let MatcherType::Regex {ref regex_string} = n.normalizer_type {
+                if let Err(e) = RegexBuilder::new(regex_string).build() {
+                    return Err(format!("Invalid regex string provided to normalizer for payee [{}] in account [{}]: {}",
+                                       n.payee_id, a_id, e));
+                }
+            }
+            if let Option::None = a.payees.get(&n.payee_id) {
+                return Err(format!("Invalid payee id [{}] used in a payee normalizer in account [{}]",
+                                   n.payee_id, a_id));
+            }
+        }
+    }
+    Ok(())
 }
 
 #[derive(Debug, Deserialize)]
@@ -187,6 +240,22 @@ impl Keyed<String> for Payee {
     fn key(&self) -> String {
         self.id.to_owned()
     }
+}
+
+fn validate_payees(c: &ConfigFile) -> Result<(), String> {
+    for (a_id, a) in &c.accounts {
+        for (p_id, p) in &a.payees {
+            if let Option::Some(ref category_ids) = p.category_ids {
+                for c_id in category_ids {
+                    if let Option::None = c.categories.get(c_id) {
+                        return Err(format!("Category [{}] does not exist. Referenced from payee [{}] in account [{}].",
+                                           c_id, p_id, a_id));
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
 }
 
 #[derive(Debug, Deserialize)]
@@ -258,6 +327,37 @@ impl Keyed<String> for FormatConfig {
     }
 }
 
+fn validate_formats(c: &ConfigFile) -> Result<(), String> {
+    for (f_id, f) in &c.formats {
+        validate_date_time_config(f, &f.date_time_config)?;
+        validate_amount_config(f, &f.amount_config)?;
+
+        if !f.field_order.contains(&f.payee_config.field_name) {
+            return Err(format!("Payee field name [{}] for format [{}] not included in field order.",
+                               f.payee_config.field_name, f_id));
+        }
+        if let Option::Some(ref status_config) = f.status_config {
+            if !f.field_order.contains(&status_config.field_name) {
+                return Err(format!("Status field name [{}] for format [{}] not included in field order.",
+                                   status_config.field_name, f_id));
+            }
+        }
+        if let Option::Some(ref memo_config) = f.memo_config {
+            if !f.field_order.contains(&memo_config.field_name) {
+                return Err(format!("Status field name [{}] for format [{}] not included in field order.",
+                                   memo_config.field_name, f_id));
+            }
+        }
+        if let Option::Some(ref category_config) = f.category_config {
+            if !f.field_order.contains(&category_config.field_name) {
+                return Err(format!("Status field name [{}] for format [{}] not included in field order.",
+                                   category_config.field_name, f_id));
+            }
+        }
+    }
+    Ok(())
+}
+
 #[derive(Debug, Deserialize)]
 pub enum DataFormat {
     #[serde(rename = "csv")]
@@ -278,6 +378,20 @@ pub struct DateTimeConfig {
     pub deliminator: Option<String>,
 }
 
+fn validate_date_time_config(f: &FormatConfig, d: &DateTimeConfig) -> Result<(), String> {
+    if !f.field_order.contains(&d.date_field) {
+        return Err(format!("Date field name [{}] for format [{}] not included in field order.",
+                           d.date_field, f.id));
+    }
+    if let Option::Some(ref time_field) = d.time_field {
+        if !f.field_order.contains(time_field) {
+            return Err(format!("Time field name [{}] for format [{}] not included in field order.",
+                               time_field, f.id));
+        }
+    }
+    Ok(())
+}
+
 #[derive(Debug, Deserialize)]
 pub struct PayeeConfig {
     #[serde(rename = "fieldName")]
@@ -288,6 +402,38 @@ pub struct PayeeConfig {
 pub struct AmountConfig {
     #[serde(rename = "format")]
     pub format: AmountFormat,
+}
+
+fn validate_amount_config(f: &FormatConfig, a: &AmountConfig) -> Result<(), String> {
+    match a.format {
+        AmountFormat::SingleAmountField(ref c) => {
+            if !f.field_order.contains(&c.field_name) {
+                return Err(format!("Amount field name [{}] for format [{}] not included in field order.",
+                                   c.field_name, f.id));
+            }
+        },
+        AmountFormat::SeparateDebitCreditFields(ref c) => {
+            if !f.field_order.contains(&c.debit_field) {
+                return Err(format!("Debit field name [{}] for format [{}] not included in field order.",
+                                   c.debit_field, f.id));
+            }
+            if !f.field_order.contains(&c.credit_field) {
+                return Err(format!("Credit field name [{}] for format [{}] not included in field order.",
+                                   c.credit_field, f.id));
+            }
+        },
+        AmountFormat::TransactionTypeAndAmountFields(ref c) => {
+            if !f.field_order.contains(&c.amount_field) {
+                return Err(format!("Amount field name [{}] for format [{}] not included in field order.",
+                                   c.amount_field, f.id));
+            }
+            if !f.field_order.contains(&c.transaction_type_field) {
+                return Err(format!("Transaction type field name [{}] for format [{}] not included in field order.",
+                                   c.transaction_type_field, f.id));
+            }
+        },
+    }
+    Ok(())
 }
 
 #[derive(Debug, Deserialize)]
